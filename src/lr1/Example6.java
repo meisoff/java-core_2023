f709f6513f50b43dd789b7c5108e4515f926afb7
meisoff <kalyrginwot@mail.ru>
2023-03-19 15:05:12 +0500

    Initial commit

---
+++ src/lr1/Example6.java
@@ -0,0 +1,22 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите фамилию: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Введите имя: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Введите отчество: ");
+        String middleName = scanner.nextLine();
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Hello " + fullName);
+    }
+}
+
